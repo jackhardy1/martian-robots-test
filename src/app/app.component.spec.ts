853a70063d1d1a0c0b902c01500881cf811a9f6a
Jack Hardy <jackhardy1@gmail.com>
2018-02-09 13:02:57 +0000

    added three more tests, covering moving forward in every direction

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -28,5 +28,38 @@ describe('AppComponent', () => {
     
     expect(robot.currentYPosition).toBe(4);
   }))
+
+  it('robot can move forward south', async(() => {    
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    
+    var robot = new Robot(4, 3, "S");
+
+    app.moveForward(robot);
+
+    expect(robot.currentYPosition).toBe(2);
+  }))
+    
+  it('robot can move forward east', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    
+    var robot = new Robot(4,3,"E");
+
+    app.moveForward(robot);
+
+    expect(robot.currentXPosition).toBe(5);
+  }))
+  
+  it('robot can move forward west', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    
+    var robot = new Robot(4,3,"W");
+
+    app.moveForward(robot);
+
+    expect(robot.currentXPosition).toBe(3);
+  }))
   
 });
